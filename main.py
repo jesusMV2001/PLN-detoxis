38a24d6cd043d7583f14b29d27855be593204579
jesus <jmv00037@red.ujaen.es>
2024-05-05 13:52:16 +0200

    XGBoost: score: 0.36202981429591824

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data_loader import load_data
 from text_preprocessing import clean_text, prepare_features
-from train_model import train_random_forest, evaluate_model
+from train_model import train_xgboost, evaluate_model
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import RandomOverSampler
 
@@ -20,5 +20,5 @@ ros = RandomOverSampler(random_state=42)
 X_resampled, y_resampled = ros.fit_resample(X_train, y_train)
 
 # Entrenar y evaluar el modelo utilizando los datos sobremuestreados
-model = train_random_forest(X_resampled, y_resampled)
+model = train_xgboost(X_resampled, y_resampled)
 evaluate_model(model, X_test, y_test)
--- train_model.py
+++ train_model.py
@@ -1,11 +1,12 @@
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 from sklearn.metrics import classification_report, confusion_matrix, f1_score
 
 
-def train_random_forest(X_train, y_train):
-    """ Entrena un modelo de bosque aleatorio con ajustes para manejar desbalance de clases y parámetros ajustados. """
-    model = RandomForestClassifier(n_estimators=200, max_depth=15, min_samples_split=10,
-                                   min_samples_leaf=5, random_state=42, class_weight="balanced")
+def train_xgboost(X_train, y_train):
+    """ Entrena un modelo XGBoost con ajustes para manejar desbalance de clases. """
+    model = XGBClassifier(n_estimators=100, max_depth=6, learning_rate=0.1, subsample=0.8,
+                          scale_pos_weight=1, use_label_encoder=False, eval_metric='mlogloss',
+                          random_state=42)
     model.fit(X_train, y_train)
     return model
 
