775f973129b91722eb0f3a79d110ed326987a526
jesus <jmv00037@red.ujaen.es>
2024-05-05 17:34:51 +0200

    intentando mejorar logisticRegresion.py

--- logisticRegresion.py
+++ logisticRegresion.py
@@ -1,15 +1,28 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
 from sklearn.utils.class_weight import compute_class_weight
 import numpy as np
 
 
-def train_logistic_regression(X_train, y_train):
-    """ Entrena un modelo de regresión logística con pesos de clase ajustados para manejar desbalance de clases. """
+def train_logistic_regression(x_train, y_train):
+    """ Entrena un modelo de regresión logística con ajustes óptimos para manejar desbalance de clases. """
     # Calcular los pesos para las clases
     classes = np.unique(y_train)
     weights = compute_class_weight(class_weight='balanced', classes=classes, y=y_train)
     class_weights = dict(zip(classes, weights))
 
+    # Configuración para la búsqueda de hiperparámetros
     model = LogisticRegression(random_state=42, max_iter=1000, class_weight=class_weights)
-    model.fit(X_train, y_train)
-    return model
+    param_grid = {
+        'C': [0.01, 0.1, 1, 10, 100],
+        'penalty': ['l1', 'l2'],
+        'solver': ['liblinear']  # liblinear trabaja bien con l1 y l2
+    }
+    grid_search = GridSearchCV(model, param_grid, cv=5, scoring='f1_macro')
+    grid_search.fit(x_train, y_train)
+
+    print("Mejores parámetros:", grid_search.best_params_)
+    print("Mejor score F1-macro:", grid_search.best_score_)
+
+    # Retorna el modelo con los mejores parámetros
+    return grid_search.best_estimator_
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from XGBoost import train_xgboost
+from logisticRegresion import train_logistic_regression
 from data_loader import load_data
 from predict import generate_predictions
 from text_preprocessing import clean_text, prepare_features
 from sklearn.model_selection import train_test_split
-from sklearn.utils.class_weight import compute_class_weight
-import numpy as np
-
 from train_model import evaluate_model
 
 # Cargar los datos
@@ -19,13 +16,8 @@ labels = data['label']
 # Dividir los datos
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
-# Calcular los pesos para las clases
-class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
-weights = {i: class_weights[i] for i in range(len(class_weights))}
-sample_weights = np.array([weights[i] for i in y_train])
-
 # Entrenar y evaluar el modelo utilizando los datos y los pesos de las clases
-model = train_xgboost(X_train, y_train, sample_weights)
+model = train_logistic_regression(X_train, y_train)
 evaluate_model(model, X_test, y_test)
 
 generate_predictions(model, 'Data/Detoxis_test_kaggle.csv', 'submission.csv')
