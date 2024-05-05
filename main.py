0552d51101f1fcf3d98397a4f28e050539329237
jesus <jmv00037@red.ujaen.es>
2024-05-05 16:19:59 +0200

    creando submission.csv

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from data_loader import load_data
+from logisticRegresion import train_logistic_regression
 from predict import generate_predictions
+from randomForest import train_random_forest
 from text_preprocessing import clean_text, prepare_features
-from train_model import train_xgboost, evaluate_model
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import RandomOverSampler
-import pickle
 
 # Cargar los datos
 data = load_data('Data/Detoxis_train_kaggle.csv')
@@ -22,7 +22,7 @@ ros = RandomOverSampler(random_state=42)
 X_resampled, y_resampled = ros.fit_resample(X_train, y_train)
 
 # Entrenar y evaluar el modelo utilizando los datos sobremuestreados
-model = train_xgboost(X_resampled, y_resampled)
+model = train_logistic_regression(X_resampled, y_resampled)
 #evaluate_model(model, X_test, y_test)
 
 generate_predictions(model, 'Data/Detoxis_test_kaggle.csv', 'submission.csv')
--- train_model.py
+++ train_model.py
@@ -1,19 +1,9 @@
-from xgboost import XGBClassifier
 from sklearn.metrics import classification_report, confusion_matrix, f1_score
 
 
-def train_xgboost(X_train, y_train):
-    """ Entrena un modelo XGBoost con ajustes para manejar desbalance de clases. """
-    model = XGBClassifier(n_estimators=100, max_depth=6, learning_rate=0.1, subsample=0.8,
-                          scale_pos_weight=1, use_label_encoder=False, eval_metric='mlogloss',
-                          random_state=42)
-    model.fit(X_train, y_train)
-    return model
-
-
-def evaluate_model(model, X_test, y_test):
+def evaluate_model(model, x_test, y_test):
     """ Evalúa el modelo con el conjunto de prueba y muestra métricas de rendimiento. """
-    y_pred = model.predict(X_test)
+    y_pred = model.predict(x_test)
     print("Informe de clasificación:")
     print(classification_report(y_test, y_pred, zero_division=0))
     print("Matriz de confusión:")
