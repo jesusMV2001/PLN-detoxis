4af77acee811f3c2817c522e97fc0cbc7dfd6006
jesus <jmv00037@red.ujaen.es>
2024-05-05 17:03:33 +0200

    intentando mejorar xgboost

--- XGBoost.py
+++ XGBoost.py
@@ -1,10 +1,9 @@
 from xgboost import XGBClassifier
 
 
-def train_xgboost(X_train, y_train):
-    """ Entrena un modelo XGBoost con ajustes para manejar desbalance de clases. """
-    model = XGBClassifier(n_estimators=100, max_depth=6, learning_rate=0.1, subsample=0.8,
-                          scale_pos_weight=1, use_label_encoder=False, eval_metric='mlogloss',
-                          random_state=42)
-    model.fit(X_train, y_train)
+def train_xgboost(x_train, y_train, sample_weights):
+    model = XGBClassifier(n_estimators=100, max_depth=9, learning_rate=0.05, subsample=0.8,
+                          use_label_encoder=False, eval_metric='mlogloss', random_state=42,
+                          objective='multi:softmax')
+    model.fit(x_train, y_train, sample_weight=sample_weights)
     return model
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from XGBoost import train_xgboost
 from data_loader import load_data
-from logisticRegresion import train_logistic_regression
 from predict import generate_predictions
 from text_preprocessing import clean_text, prepare_features
 from sklearn.model_selection import train_test_split
-from imblearn.over_sampling import RandomOverSampler
+from sklearn.utils.class_weight import compute_class_weight
+import numpy as np
 
 from train_model import evaluate_model
 
@@ -18,12 +19,13 @@ labels = data['label']
 # Dividir los datos
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
-# Aplicar sobremuestreo a las clases minoritarias
-ros = RandomOverSampler(random_state=42)
-X_resampled, y_resampled = ros.fit_resample(X_train, y_train)
+# Calcular los pesos para las clases
+class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
+weights = {i: class_weights[i] for i in range(len(class_weights))}
+sample_weights = np.array([weights[i] for i in y_train])
 
-# Entrenar y evaluar el modelo utilizando los datos sobremuestreados
-model = train_logistic_regression(X_resampled, y_resampled)
+# Entrenar y evaluar el modelo utilizando los datos y los pesos de las clases
+model = train_xgboost(X_train, y_train, sample_weights)
 evaluate_model(model, X_test, y_test)
 
 generate_predictions(model, 'Data/Detoxis_test_kaggle.csv', 'submission.csv')
